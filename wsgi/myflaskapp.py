faa342043445611dafa48dc1c52762e2c59ec922
Samuel Mendenhall <smendenh@redhat.com>
2013-06-08 17:33:43 -0400

    Adding a route

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/fuelapp")
+def hello():
+    return "For all of your car comparison needs!"
+
 if __name__ == "__main__":
     app.run()
 
