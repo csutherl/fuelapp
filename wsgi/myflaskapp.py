ef2d620302cc9cea543e2823c1c72df3be29d1c5
Samuel Mendenhall <smendenh@redhat.com>
2013-06-09 21:52:10 -0400

    Adding in the Skeleton framework, and playing with jade a bit

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 @app.route("/foo")
-def hello():
+def foo():
     return "For all of your car comparison needs!"
+
+# Testing where the templates dir should go, this works
+@app.route('/hi/')
+@app.route('/hi/<name>')
+def hi(name=None):
+    return render_template('test/hello.html', name=name)
+
 
 if __name__ == "__main__":
     app.run()
